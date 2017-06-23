258fc160eabdb89631bbc5dce942d34fad6e883a
Romain Francois <romain@thinkr.fr>
2017-06-23 12:51:39 +0200

    update departements

--- data-raw/departements.R
+++ data-raw/departements.R
@@ -1,5 +1,7 @@
 library(sf)
 
-departements <- st_read( "data-raw/map/departements-20140306-50m.shp" )
+departements <- st_read( "data-raw/map/departements-20140306-50m.shp" ) %>%
+  select(-nuts3,-wikipedia) %>%
+  mutate_at( vars(nom,code_insee), as.character )
 
-use_data( departements, overwrite = TRUE )
+devtools::use_data( departements, overwrite = TRUE )
