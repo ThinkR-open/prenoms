4cc2f329e7ef0fa03abc429350b4a6df9d3229e7
Romain Francois <romain@thinkr.fr>
2017-06-23 13:07:49 +0200

    encoding latin

--- data-raw/departements.R
+++ data-raw/departements.R
@@ -1,6 +1,6 @@
 library(sf)
 
-departements <- st_read( "data-raw/map/departements-20140306-50m.shp" ) %>%
+departements <- st_read( "data-raw/map/departements-20140306-50m.shp", stringsAsFactors = FALSE ) %>%
   select(-nuts3,-wikipedia) %>%
   mutate_at( vars(nom,code_insee), as.character )
 
