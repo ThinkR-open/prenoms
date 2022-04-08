870f5779f0b20bdd564e5048d78270fa32769434
ALanguillaume <antoinelanguillaume@gmail.com>
2022-04-08 10:09:43 +0200

    Update github actions yaml
    Check was failing on ubuntu 20.04 for r-devel because of the curl
    package. It was not clear if it was because the curl system binary or
    the {curl} pacakge was not installed properly.
    
    Setup the last version of r-lib github actions to try to resolve this
    issue.

--- dev/dev_history_package.R
+++ dev/dev_history_package.R
@@ -9,3 +9,8 @@ rcmdcheck::rcmdcheck()
 ### Setup ----------------------------------------------------------------------
 
 usethis::use_build_ignore("dev/")
+
+## CI
+usethis::use_github_action_check_standard(
+  save_as = "check-standard.yaml"
+)
