97143c23c5c87edf8a4bd5a763f23f24204348dd
ALanguillaume <antoinelanguillaume@gmail.com>
2022-04-08 10:06:26 +0200

    Add dev_history.R
    To keep a record of dev actions.

---
+++ dev/dev_history_package.R
@@ -0,0 +1,11 @@
+
+### Daily Dev ------------------------------------------------------------------
+
+attachment::att_amend_desc()
+
+devtools::check()
+rcmdcheck::rcmdcheck()
+
+### Setup ----------------------------------------------------------------------
+
+usethis::use_build_ignore("dev/")
