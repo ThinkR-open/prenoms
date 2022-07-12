c4ff152cb6e8ccdfb42b33faa3951790995427ec
florence <florence@thinkr.fr>
2022-07-12 12:29:13 +0000

    Ass unit tests
    Why?
    To make sure the examples presented in the README work
    
    What?
    add tests on s3_class and column names from the objects produced in the examples in test-prenoms.R
    
    Issue #19

--- tests/testthat/test-prenoms.R
+++ tests/testthat/test-prenoms.R
@@ -13,3 +13,95 @@ test_that("prenoms is loaded works", {
   expect_equal(names(prenoms_france),c("year", "sex", "name", "n", "prop"))
 
 })
+test_that("examples in README work", {
+  library(prenoms)
+  library(dplyr)
+  library(tidyr)
+  library(purrr)
+  library(ggplot2)
+
+  team_members <- tribble(
+    ~name,      ~sex,
+    "Colin",     "M",
+    "Diane",     "F",
+    "Sébastien", "M",
+    "Cervan",    "M",
+    "Vincent",   "M",
+    "Margot",    "F",
+    "Estelle",  " F",
+    "Arthur",    "M",
+    "Antoine",   "M",
+    "Florence",  "F",
+    "Murielle",  "F"
+  )
+
+  get_thinkr_team_name_data <- function(
+    prenoms_df,
+    team_members_df
+  ) {
+    prenoms_df %>%
+      # Get data corresponding only to team member names
+      inner_join(
+        team_members,
+        by = c("name", "sex")
+      ) %>%
+      # Add missing combination for name x year
+      complete(
+        name = team_members$name,
+        year = 1900:2020,
+        fill = list( n = 0, prop = 0 )
+      ) %>%
+      group_by(name, year, sex) %>%
+      summarise(
+        n = sum(n),
+        .groups = "drop"
+      ) %>%
+      arrange(year) %>%
+      # If sex is not define (NA) we assumed it was
+      # the same as the corresponding team member's
+      mutate(
+        sex = map2_chr(
+          sex,
+          name,
+          function(
+            sex,
+            name
+          ) {
+            ifelse(
+              is.na(sex) & name %in% team_members$name,
+              team_members$sex[team_members$name == name],
+              sex
+            )
+          }
+        )
+      )
+  }
+
+  data(prenoms_france)
+
+  thinkrs <- get_thinkr_team_name_data(
+    prenoms_df = prenoms_france,
+    team_members_df = team_members
+  )
+
+  ggplot1 <- thinkrs %>%
+    ggplot() +
+    aes(x = year, y = n, color = name) +
+    geom_line() +
+    scale_x_continuous( breaks = seq(1900, 2020, by = 10) ) +
+    labs(title = "ThinkR's team names evolution in France") +
+    theme_bw()
+
+  # tests on `thinkrs`
+  expect_s3_class(object = thinkrs, class = "tbl")
+  expect_equal(names(thinkrs), c("name", "year", "sex", "n"))
+  expect_type(object = thinkrs[["name"]], type = "character")
+  expect_type(object = thinkrs[["year"]], type = "double")
+  expect_type(object = thinkrs[["sex"]], type = "character")
+  expect_type(object = thinkrs[["n"]], type = "integer")
+
+  #tests on `ggplot1`
+  expect_s3_class(object = ggplot1, class = "ggplot")
+
+
+})
